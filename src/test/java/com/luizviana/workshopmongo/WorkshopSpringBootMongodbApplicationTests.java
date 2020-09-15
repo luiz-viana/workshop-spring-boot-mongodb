cc466b23c11582fa03833021e92e48b3f9d142e9
luiz-viana <luiz9505@hotmail.com>
2020-09-15 11:55:37 -0300

    First Commit, created project

---
+++ src/test/java/com/luizviana/workshopmongo/WorkshopSpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizviana.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
